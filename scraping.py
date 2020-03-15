82fa10fb371f95a2ee6d66b4280b7fecf6e57fa0
saadashraf <saadbinashraf14@gmail.com>
2020-03-15 15:16:23 +0600

    Added author info. COmpleted local code

--- scraping.py
+++ scraping.py
@@ -9,36 +9,49 @@ website = request.content
 soup_website = BeautifulSoup(website , 'html5lib')
 homepage_news = soup_website.find_all("h3" , class_ = "fc-item__title")
 
-article_title = []
 article_link = []
+article_title = []
+article_author = []
 article_content = []
-article_content_extended = []
-article_title_extended = []
 
 for i in range(len(homepage_news)):
     if ("coronavirus" not in homepage_news[i].find("a")["href"] and
         "covid-19" not in homepage_news[i].find("a")["href"]):
         continue
+
+    #Link
     link = homepage_news[i].find("a")["href"]
     article_link.append(link)
+    print(link)
 
+    #Title
     article = requests.get(link)
     article_webpage  = article.content
     soup_article = BeautifulSoup(article_webpage , "html5lib")
-    header = soup_article.find("h1" , class_ = "content__headline")
-    print(link)
-    print(header.get_text())
-    article_title_extended.append(header.get_text())
+    title = soup_article.find("h1" , class_ = "content__headline")
+    if title is not None:
+        print(title.get_text())
+        article_title.append(title.get_text())
 
+    #author
+    paragraphs = soup_article.find_all("p")
+    if paragraphs[1] is not None:
+        name = paragraphs[1].get_text()
+        print(name)
 
-    title = homepage_news[i].find_all("span")[0].get_text()
-    article_title.append(title)
-
-    content = homepage_news[i].find_all("span")[1].get_text()
+    #content
+    list_of_paragraphs = []
+    
+    for j in range(0 , len(paragraphs)):
+        if(j == 1):
+            continue
+        p = paragraphs[j]
+        paragraph = p.get_text()
+        list_of_paragraphs.append(paragraph)
+        content = "\n".join(list_of_paragraphs)
+    
     article_content.append(content)
-
-    paragraph = soup_article.find_all("p")
-    print(paragraph[0].get_text())
+    print(content)
 
     print(i)
 
